473e0cc1be8a243c2b97fb530914b9cbdba26a59
Nils Voigt Dahl <nils.voigt.dahl@ericsson.com>
2018-01-24 11:31:46 +0100

    Labb klar på riktigt

--- labb1/GeneralWordCounter.java
+++ labb1/GeneralWordCounter.java
@@ -3,9 +3,12 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GeneralWordCounter implements TextProcessor {	
-	private Map<String,Integer> generalWordMap = new HashMap<String,Integer>();
+	private Map<String,Integer> generalWordMap = new TreeMap<String,Integer>();
 	private Set<String> stopwords = new HashSet<String>();
 	private Set<String> commonWords = new HashSet<String>();	//Mängd med ord som förekommer >200ggr
 	
@@ -27,6 +30,14 @@ public class GeneralWordCounter implements TextProcessor {
 	
 	@Override
 	public void report() {
-		System.out.print("Ord som förekommer fler än 200 ggr: " + commonWords.toString());
+		// Uppgift D7:
+		//System.out.print("Ord som förekommer fler än 200 ggr: " + commonWords.toString());
+		Set<Map.Entry<String, Integer>> wordset = generalWordMap.entrySet();
+		List<Map.Entry<String, Integer>> wordList = new ArrayList<>(wordset);
+		wordList.sort(new WordCountComparator());
+		for(int i = 0; i < 5; i++) {
+			System.out.println(wordList.get(i).toString() + " ");
+		}
+ 		
 	}
 }
--- labb1/Holgersson.java
+++ labb1/Holgersson.java
@@ -15,7 +15,7 @@ public class Holgersson {
 			"öland", "östergötland" };
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
+		long t0 = System.nanoTime();	//Tidpunkt 1
 		ArrayList<TextProcessor> list = new ArrayList<TextProcessor>();
 		
 		Scanner swScan = new Scanner(new File("undantagsord.txt"));
@@ -25,16 +25,14 @@ public class Holgersson {
 			stopwords.add(swScan.next());
 		}
 		
-		TextProcessor r = new GeneralWordCounter(stopwords);
-		
-		TextProcessor p1 = new SingleWordCounter("nils");
+		/*TextProcessor p1 = new SingleWordCounter("nils");
 		TextProcessor p2 = new SingleWordCounter("norge");  
 		
-		TextProcessor p3 = new MultiWordCounter(REGIONS);
+		TextProcessor p3 = new MultiWordCounter(REGIONS);*/
 		TextProcessor p4 = new GeneralWordCounter(stopwords);
 		
-		list.add(p1);
-		list.add(p2);
+		//list.add(p1);
+		//list.add(p2);
 		
 		Scanner s = new Scanner(new File("nilsholg.txt"));
 		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
@@ -42,20 +40,20 @@ public class Holgersson {
 		while (s.hasNext()) {
 			String word = s.next().toLowerCase();
 
-			p1.process(word);
+			/*p1.process(word);
 			p2.process(word);
-			p3.process(word);
+			p3.process(word);*/
 			p4.process(word);
 			
 		}
 
 		s.close();
 
-		p1.report();
+		/*p1.report();
 		p2.report();
-		p3.report();
+		p3.report();*/
 		p4.report();
-		
-		
+		long t1 = System.nanoTime();
+		System.out.println("tid: " + (t1 - t0) / 1000000.0 +  " ms");
 	}
 }
