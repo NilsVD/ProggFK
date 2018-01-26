1ecc9c837116b6646a2ca4865fe8ca9172656a3f
Nils Voigt Dahl <nils.voigt.dahl@ericsson.com>
2018-01-26 13:54:33 +0100

    Svar på D3

---
+++ labb2/queue_singlelinkedlist/FifoQueue.java
@@ -0,0 +1,91 @@
+package queue_singlelinkedlist;
+import java.util.*;
+
+public class FifoQueue<E> extends AbstractQueue<E> implements Queue<E> {
+	private QueueNode<E> last;
+	private int size;
+
+	public FifoQueue() {
+		super();
+		last = null;
+		size = 0;
+	}
+
+	/**	
+	 * Inserts the specified element into this queue, if possible.
+	 * post:	The specified element is added to the rear of this queue
+	 * @param	e the element to insert
+	 * @return	true if it was possible to add the element 
+	 * 			to this queue, else false
+	 */
+	public boolean offer(E e) {
+		QueueNode<E> n = new QueueNode<E>(e);
+		if(size == 0) {
+			n.next = n;
+		} else {
+			n.next = last.next;
+			last.next = n;
+		}
+		last = n;
+		size++;
+		return true;
+	}
+	
+	/**	
+	 * Returns the number of elements in this queue
+	 * @return the number of elements in this queue
+	 */
+	public int size() {		
+		return size;
+	}
+	
+	/**	
+	 * Retrieves, but does not remove, the head of this queue, 
+	 * returning null if this queue is empty
+	 * @return 	the head element of this queue, or null 
+	 * 			if this queue is empty
+	 */
+	public E peek() {
+		if(last == null) {
+			return null;	//	Är detta nödvändigt eller är last.next.element == null?
+		} else {
+		return last.next.element;
+		}
+	}
+
+	/**	
+	 * Retrieves and removes the head of this queue, 
+	 * or null if this queue is empty.
+	 * post:	the head of the queue is removed if it was not empty
+	 * @return 	the head of this queue, or null if the queue is empty 
+	 */
+	public E poll() {
+		if(last == null) {
+			return null;	//	Är detta nödvändigt eller är last.next.element == null?
+		} else {
+			QueueNode<E> r = last.next;
+			last.next = last.next.next;
+			size--;
+			return r.element;
+		}
+	}
+	
+	/**	
+	 * Returns an iterator over the elements in this queue
+	 * @return an iterator over the elements in this queue
+	 */	
+	public Iterator<E> iterator() {
+		return null;
+	}
+	
+	private static class QueueNode<E> {
+		E element;
+		QueueNode<E> next;
+
+		private QueueNode(E x) {
+			element = x;
+			next = null;
+		}
+	}
+
+}
