9c02aee42ddbfb0d56c9b0f2e458443de095f5a3
Nils Voigt Dahl <nils.voigt.dahl@ericsson.com>
2018-02-05 14:54:22 +0100

    Labb 3 Done

--- lab3/BookReaderControl.java
+++ lab3/BookReaderControl.java
@@ -10,9 +10,13 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -56,19 +60,28 @@ public class BookReaderControl extends Application {
 		Button AlphButton = new Button ("Alphabetic");
 		Button FreqButton = new Button ("Frequency");
 		TextField search = new TextField("");
+		search.setPromptText("Search...");
 		Button FindButton = new Button ("Find");
 		hbox.getChildren().addAll(AlphButton, FreqButton, search, FindButton);
 		root.setBottom(hbox);
+		Alert alert = new Alert(AlertType.WARNING, "The word can not be found");
 		
 		FreqButton.setOnAction(event -> words.sort((e1,e2) -> e2.getValue() - e1.getValue() ));
 		AlphButton.setOnAction(event -> words.sort((e1,e2) -> e1.getKey().compareTo(e2.getKey()) ));
 		FindButton.setOnAction(event -> {
-			String searchWord = search.getText();
+			String searchWord = search.getText().toLowerCase().trim();
 			for(int i=0 ; i<words.size();i++) {
 				if(words.get(i).getKey().equals(searchWord)){
-				listView.scrollTo(words.get(i));	
+					listView.scrollTo(words.get(i));	
 				//System.out.println("Hittad");
+				} else if (i == words.size()-1) {
+					alert.showAndWait();
 				}
+			}
+		});
+		search.setOnKeyPressed(event -> {
+			if(event.getCode() == KeyCode.ENTER) {
+				FindButton.fire();
 			}
 		});
 
